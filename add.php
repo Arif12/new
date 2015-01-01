eabb5dc54e2452ff7e510bd3d5a698e6bb7b720e
Arif12 <arif.uisc@gmail.com>
2015-01-01 16:03:27 +0600

    Update add.php

--- add.php
+++ add.php
@@ -1,18 +1,42 @@
 <?php
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
 $fullname=$_POST['fullname'];
-echo $fullname;
-$hobby= $_POST['hobby'];
-$hobby= implode(",",$_POST['hobby']);
+//print_r($fullname);
+//$hobby= $_POST['hobby'];
+$hobby='';
+
+if (array_key_exists('hobby_reading',$_POST) && !empty ($_POST['hobby_reading'])) {
+    $hobby.= $hobby_reading.',';
+}
+
+if (array_key_exists('hobby_travel',$_POST) && !empty ($_POST['hobby_travel'])) {
+     $hobby.= $_POST['hobby_travel'] .',';
+}
+
+if (array_key_exists('hobby_movie',$_POST) && !empty ($_POST['hobby_movie'])){
+    $hobby.= $_POST['hobby_movie'].',';
+}
+
+if(array_key_exists('hobby_cric',$_POST) && !empty($_POST['hobby_cric'])) {
+    $hobby.= $_POST['hobby_cric'].',';
+}
+$hobby=substr($hobby,0,-1);
 echo $hobby;
-$preferredjob=implode(",",['preferredjob']);
+echo "<br/>";
+$food= implode(",",$_POST['food']);
+echo $food;
+echo "<br/>";
+$preferredjob=implode(",",$_POST['cities']);
 echo $preferredjob;
 $link= mysqli_connect("localhost",
 "root",
 "lict@2",
 "multiple");
 
-var_dump($link);
 
-//$query = "INSERT INTO `multiple`.`users` (`id`, `fullname`,`hobby`) VALUES ('NULL', '$fullnamename',$hobby')";
-//mysqli_query($link, $query);
+$query = "INSERT INTO `multiple`.`users` (`id`, `fullname`, `hobby`, `food`, `preferredjob`, `created`, `modified`) VALUES (NULL, '$fullname', '$hobby', '$food', '$preferredjob', '', '')";;
+mysqli_query($link, $query);
 //header('location:list.php');
+?>
